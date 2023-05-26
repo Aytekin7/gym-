e9b0f46b0d9f55223f0d1d516c7e50e482f118a5
Ehmedova Aytekin <6y7m72xcode.edu.az>
2023-05-26 19:25:51 +0400

    second file

--- client/src/pages/Blog/Blog.jsx
+++ client/src/pages/Blog/Blog.jsx
@@ -5,7 +5,7 @@ function Blog() {
   return (
   <>
   <div className={style.blog}>
-    <h1 style={{color:'white',paddingTop:'20%',marginLeft:'40%'}}>Huge Transaction in last Week</h1>
+    <h1 style={{color:'white',marginLeft:'40%',}}>Huge Transaction in last Week</h1>
  <p style={{color:'white',marginLeft:'30%',marginTop:'20px'}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore<br/>magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
     <button style={{marginLeft:'40%',width:'180px',height:'40px',borderRadius:'16px',marginTop:'20px',color:'white',backgroundColor:'red'}}>BECOME A MEMBER</button>
   </div> 
